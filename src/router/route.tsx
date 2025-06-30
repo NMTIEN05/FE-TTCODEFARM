a7a21ecf0c68b3f9fdd17ebf75fdf3d0a3cf62cf
Minq05 <hoangquan22022005@gmail.com>
2025-06-30 21:59:32 +0700

    forgot password

--- src/router/route.tsx
+++ src/router/route.tsx
@@ -6,6 +6,7 @@ import ProtectedRoute from "../components/ProtectedRoute";
 import Admin from "../admin";
 import Register from "../pages/auth/Register";
 import EmailOTPVerification from "../pages/auth/EmailOTPVerification";
+import ForgotPassword from "../pages/auth/ForgotPassword";
 import DetelProduct from "../pages/detel/DetelProduct";
 import Cart from "../pages/Cart/Cart";
 import Allproduct from "../pages/ProductAll/Allproduct";
@@ -46,6 +47,7 @@ export const router = createBrowserRouter([
   { path: "/auth/register", element: <Register /> },
   { path: "/auth/login", element: <Login /> },
   { path: "/auth/verify-otp", element: <EmailOTPVerification /> },
+  { path: "/auth/forgot-password", element: <ForgotPassword /> },
   {
     path: "/admin",
     element: (
